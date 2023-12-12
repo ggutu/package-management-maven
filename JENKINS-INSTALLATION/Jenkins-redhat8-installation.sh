d99796eeab59b0c1845fa069feae9f3a0cc8c9a9
ggutu <151479796+ggutu@users.noreply.github.com>
2023-12-11 20:46:59 -0600

    Update Jenkins-redhat8-installation.sh

--- JENKINS-INSTALLATION/Jenkins-redhat8-installation.sh
+++ JENKINS-INSTALLATION/Jenkins-redhat8-installation.sh
@@ -10,14 +10,15 @@
 # You can execute this script as user-data when launching your EC2 VM.
 sudo timedatectl set-timezone America/New_York
 sudo hostnamectl set-hostname jenkins
+sudo yum install wget tree vim git nano unzip -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo yum upgrade
+sudo yum upgrade -y
 # Add required dependencies for the jenkins package
-sudo yum install java-11-openjdk
-sudo yum install jenkins
-sudo systemctl daemon-reload
+sudo yum install java-11-openjdk -y
+sudo yum install jenkins -y
+sudo systemctl daemon-reload -
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 sudo systemctl status jenkins
